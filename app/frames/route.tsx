161796e7d5b8aca39022fec7d2171201f18c8a77
MassHesteria <brad.hester@gmail.com>
2024-04-05 23:05:08 -0500

    Added text field

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -1,21 +1,34 @@
 /* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next";
 import { frames } from "./frames";
+import { getHostName} from "../data";
  
 const handleRequest = frames(async (ctx) => {
+  //console.log(JSON.stringify(ctx))
   return {
     image: (
-      <span>
-        {ctx.pressedButton
-          ? `I clicked ${ctx.searchParams.value}`
-          : `Click some button`}
-      </span>
+      <div tw="flex flex-col">
+        <span>
+          {ctx.pressedButton
+            ? `I clicked ${ctx.searchParams.value}`
+            : `Click some button`}
+        </span>
+        <span>
+          {ctx.message?.inputText
+            ? `Text: ${ctx.message.inputText}`
+            : 'no text'}
+        </span>
+      </div>
     ),
+    imageOptions: {
+        aspectRatio: '1:1'
+    },
+    textInput: 'Move:',
     buttons: [
-      <Button action="post" target={{ query: { value: "Yes" } }}>
+      <Button action="post" target={getHostName() + "/frames?value=Yes"}>
         Say Yes
       </Button>,
-      <Button action="post" target={{ query: { value: "No" } }}>
+      <Button action="post" target={getHostName() + "/frames?value=No"}>
         Say No
       </Button>,
     ],
