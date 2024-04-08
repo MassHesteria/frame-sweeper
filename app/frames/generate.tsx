40ac63e4b70f8fe160ba86b333688bb2ef0afe7d
MassHesteria <brad.hester@gmail.com>
2024-04-07 20:16:45 -0500

    Fixed tailwind warning

--- app/frames/generate.tsx
+++ app/frames/generate.tsx
@@ -4,10 +4,10 @@ const IntroPage = () => {
   return (
     <div tw="flex bg-orange-200 h-full w-full">
       <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-center p-8">
-        <h2 tw="flex flex-col font-bold tracking-tight text-left">
-          <span tw="pb-5 text-8xl text-amber-900">Minesweeper</span>
+        <h2 tw="flex flex-col font-bold tracking-tight text-8xl text-left text-amber-900">
+          <span tw="pb-5">Minesweeper</span>
           <span tw="pb-6 text-5xl text-amber-700">by MassHesteria</span>
-          <span tw="pt-16 text-6xl text-amber-900">Follow to Play!</span>
+          <span tw="pt-16 text-6xl">Follow to Play!</span>
         </h2>
       </div>
     </div>
