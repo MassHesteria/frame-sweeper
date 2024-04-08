8dcfe1d4596070036f0df18650202b1cb6093bcd
MassHesteria <brad.hester@gmail.com>
2024-04-08 15:19:52 -0500

    Moved IntroPage to dedicated file

--- app/frames/generate.tsx
+++ app/frames/generate.tsx
@@ -1,18 +1,5 @@
 import { Board, Cell } from "./frames";
-
-const IntroPage = () => {
-  return (
-    <div tw="flex bg-orange-200 h-full w-full">
-      <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-center p-8">
-        <h2 tw="flex flex-col font-bold tracking-tight text-8xl text-left text-amber-900">
-          <span tw="pb-5">Minesweeper</span>
-          <span tw="pb-22 text-5xl text-amber-700">by MassHesteria</span>
-          <span tw="text-6xl">Follow to Play!</span>
-        </h2>
-      </div>
-    </div>
-  )
-}
+import { IntroPage } from "./components/intro";
 
 const Row = ({
   row,
