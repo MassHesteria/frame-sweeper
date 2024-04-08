8094045fc97b52b5dee14f0f1ec53403cd83d774
MassHesteria <brad.hester@gmail.com>
2024-04-07 20:09:41 -0500

    Host name fix?

--- app/data.ts
+++ app/data.ts
@@ -1,6 +1,10 @@
 import { headers } from "next/headers";
 
 export const getHostName = (): string => {
+  if (process.env['HOST']) {
+    console.log('using',process.env['HOST'])
+    return process.env['HOST']
+  }
   const headersList = headers();
   const host = headersList.get('x-forwarded-host');
   const proto = headersList.get('x-forwarded-proto');
